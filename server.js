da9310878f0bb67c1ecd9a9d0a62bb6f538cb94d
Pranav Pise <itspranavpise@gmail.com>
2025-06-27 15:19:15 +0530

    server endpoints implemented

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const { PORT } = require("./config");
 
 const app = express();
 app.use(express.static("wwwroot"));
+
 app.use(require("./routes/auth.js"));
+app.use(require("./routes/models.js"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
